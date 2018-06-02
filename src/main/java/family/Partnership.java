9cab6c37b836d02c906c9a70137ac1183a673b9a
johnny-dash <maodi101@gmail.com>
2018-06-02 19:34:06 +1000

    initial commitment

---
+++ src/main/java/family/Partnership.java
@@ -0,0 +1,25 @@
+package family;
+
+public class Partnership {
+    private Person mPartners[];
+    private ArrayList<Person> mChildrens;
+
+    public Partnership(){
+        this.mPartners = new Person[2];
+    }
+
+    public void setPartners() {
+
+    }
+
+    public void setChildrens() {
+        
+    }
+
+    public Person getChildrens() {
+
+    }
+
+    public 
+
+}
---
+++ src/main/java/family/Person.java
@@ -0,0 +1,32 @@
+package family;
+
+public class Person {
+    private final Gender gender;
+    private final String name;
+    private Partnership partnership[];
+
+    public Person(String name, Gender gender){
+        this.gender = gender;
+        this.name = name;
+    }
+
+    public void setChildren(Person children){
+        //children.push and pudate the spouse
+    }
+
+    public void setSpouse(){
+        
+    }
+
+    public Person getChildren() {
+
+    }
+
+    public Person getParent() {
+
+    }
+
+    public Person getSpouse() {
+
+    }
+}
